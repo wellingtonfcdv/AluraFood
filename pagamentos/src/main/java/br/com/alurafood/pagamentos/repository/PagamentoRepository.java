0a7b3c994b6f68b85a825465bdd8f48d6b9b63bd
Wellington Feijo Da Costa <>
2026-02-10 14:31:04 -0300

    Interface para a criação do crud

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/repository/PagamentoRepository.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/repository/PagamentoRepository.java
@@ -1,4 +1,7 @@
 package br.com.alurafood.pagamentos.repository;
 
-public interface PagamentoRepository {
+import br.com.alurafood.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 }
