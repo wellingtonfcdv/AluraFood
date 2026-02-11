be18ef02d6b12c72f8218d257b1d31de84abdf38
Wellington Feijo Da Costa <>
2026-02-11 09:26:11 -0300

    Métodos para realização de operações como criarPagamento, exibirTodos, exibirPorId, atualizarPagamento, excluirPagamento

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -1,4 +1,51 @@
 package br.com.alurafood.pagamentos.service;
 
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PagamentoService {
+    @Autowired
+    private PagamentoRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper; //permite transferir dados de forma facilitada entre a entidade e o DTO
+
+    //métodos baseado nas operações que serão realizados
+    public Page<PagamentoDto> obterTodos(Pageable paginacao){
+        return repository
+                .findAll(paginacao)
+                .map(p -> modelMapper.map(p, PagamentoDto.class));
+    }
+
+    //pagamento por id
+    public PagamentoDto obterPorId(Long id){
+        Pagamento pagamento = repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    //criar pagamento
+    public PagamentoDto criarPagamento(PagamentoDto dto){
+        Pagamento pagamento = modelMapper.map(dto,Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    //atualizar pagamento
+    public void excluirPagamento(Long id){
+        repository.deleteById(id);
+    }
+
+
 }
